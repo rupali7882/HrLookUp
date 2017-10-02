73cb3e3e1cd64211fc976c46f9d177daeedcfa3e
romanchsharma <romanchsharma@gmail.com>
2017-10-02 23:26:16 +0530

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Employee.delete_all
 10.times do
   Employee.create!([{
     title: Faker::Name.prefix,
-    empid: Faker::Job.title,
+    empid: Faker::Base.numerify('####'),
     firstname: Faker::Name.first_name ,
     midname: Faker::Name.last_name,
     lastname: Faker::Name.last_name,
@@ -13,8 +13,9 @@ Employee.delete_all
     city: Faker::Address.city,
     state: Faker::Address.state,
     pin: Faker::Address.postcode,
-    homenumber: Faker::PhoneNumber.phone_number,
-    mobile: Faker::PhoneNumber.cell_phone,
+    homenumber: Faker::Base.numerify('#########'),
+    mobile: Faker::Base.numerify('#########'),
+    email: Faker::Internet.email,
     joined_at: Faker::Date.between(1.year.ago, Date.today),
     birth_date:Faker::Date.birthday,
     gender: "male",
