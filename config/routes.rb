54761c39641a8eb2637e796dc72d9cb27e648e1a
Rupali Patil <rupali.dev89@gmail.com>
2017-10-03 17:28:54 +0530

    change in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => {:registrations => "registrations"}
+
   devise_scope :user do
-    root 'devise/sessions#new'
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
 
   resources :employees
