a3763e8db9fff52ec320fc608cfe19f8a840bbbb
Rupali Patil <rupali.dev89@gmail.com>
2017-10-04 11:24:05 +0530

    frontend changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-info fade in"
+      when 'success' then "alert alert-success fade in"
+      when 'error' then "alert alert-danger fade in"
+      when 'alert' then "alert alert-danger fade in"
+    end
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ApplicationRecord
+
+	def fullname
+		"#{self.title} #{self.firstname} #{self.midname} #{lastname}"
+	end
 end
