2390c4deb80dc4b104e04cb1aa2cac91172bdc43
romanchsharma <romanchsharma@gmail.com>
2017-10-04 10:30:52 +0530

    add angular js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require angular
 //= require_tree .
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+  #only use those validations in model which need to be check with database
+  #validates_presence_of attribute_names.reject { |attr| attr =~ /id|created_at|updated_at|midname|bloodtype|homenumber/i }
+  validates :empid, :email, uniqueness: true
 end
