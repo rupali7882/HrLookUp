67a831cff7fe5ea3370d84ed2ec7fd5b9a742f9a
romanchsharma <romanchsharma@gmail.com>
2017-10-05 10:52:40 +0530

    refactor routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations"}
 
   devise_scope :user do
-    authenticated :user do
       root 'home#index', as: :authenticated_root
-    end
-
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
+      root 'devise/sessions#new', as: :unauthenticated_root   
   end
 
   resources :employees
